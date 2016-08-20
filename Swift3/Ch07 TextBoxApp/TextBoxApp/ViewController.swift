07a4c2cb539c696068bae96c939c0d3e363c5731
Wendy Wise <wendylwise@gmail.com>
2016-08-20 13:44:38 -0400

    update to swift 3

---
+++ Swift3/Ch07 TextBoxApp/TextBoxApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TextBoxApp
+//
+//  Created by Wendy Wise on 8/20/16.
+//  Copyright © 2016 WisaAbility. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func changeLabelClicked(_ sender: AnyObject) {
+        myLabel.text = myTxtField.text
+    }
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var myTxtField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
