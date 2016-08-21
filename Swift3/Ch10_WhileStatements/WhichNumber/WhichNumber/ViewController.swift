c70eaf490f546a45037cfc0ce5bbf56c10256949
Wendy Wise <wendylwise@gmail.com>
2016-08-21 09:19:21 -0400

    updated to swift 3

---
+++ Swift3/Ch10_WhileStatements/WhichNumber/WhichNumber/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  WhichNumber
+//
+//  Created by Wendy Wise on 8/21/16.
+//  Copyright © 2016 WisaAbility. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var numberGuessed = 3
+    @IBOutlet weak var numFingers: UILabel!
+    @IBOutlet weak var stepperControl: UIStepper!
+    @IBOutlet weak var resultLabel: UILabel!
+  
+    @IBAction func guessButtonClick(_ sender: AnyObject) {
+        let randomNumber = Int(arc4random_uniform(6))
+        if (numberGuessed == randomNumber){
+            resultLabel.text = "Congrats!  I was holding \(numberGuessed) fingers up!"
+        }else{
+            resultLabel.text = "You guessed \(numberGuessed), but it was \(randomNumber)."
+        }
+    }
+    @IBAction func stepperClick(_ sender: AnyObject) {
+        numberGuessed = Int(stepperControl.value)
+        numFingers.text = "\(numberGuessed)"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Swift3/Ch10_WhileStatements/ch10 Playground.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var x = 5
+var y = 10
+
+//while (x < y){
+//    print("\(x) is less than \(y)")
+//    x = x+1
+//}
+
+while (x < y){
+    print("\(x) is less than \(y)")
+    y = y - 1
+}
+
+var a = 11
+
+switch a{
+case 1,3,5,7,11,13:
+    print ("value must be a prime")
+case 2, 4, 6, 8, 9, 12:
+    print ("value is not a prime")
+default:
+    print("number is not in our range")
+}
+
+var heading = "east"
+
+switch heading {
+case "north":
+    print("heading north")
+case "south":
+    print("heading south")
+case "east":
+    print("heading east")
+case "west":
+    print("heading west")
+default:
+    print("where are you headed again?")
+}
+
