56f63c9be67d0e04dc4e400b8791f94657cd06a0
Wendy Wise <wendylwise@gmail.com>
2016-09-24 13:17:27 -0400

    updated 19 for swift 3

---
+++ Swift3/Ch19_Lion/LioN/LioN/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  LioN
+//
+//  Created by Wendy Wise on 9/24/16.
+//  Copyright © 2016 WisaAbility. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController : UIViewController {
+
+    var lionDetail : Lion?
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Swift3/Ch19_Lion/LioN/LioN/LioN.swift
@@ -0,0 +1,15 @@
+//
+//  LioN.swift
+//  LioN
+//
+//  Created by Wendy Wise on 9/24/16.
+//  Copyright © 2016 WisaAbility. All rights reserved.
+//
+
+import Foundation
+
+class Lion {
+    var lionName = ""
+    var lionDescription = ""
+    var like = 1
+}
