79970d7a150284abe6137ca010edf5903cd6d19e
Wendy Wise <wendylwise@gmail.com>
2016-09-24 16:58:58 -0400

    updated chapter 21 for swift 3

---
+++ Swift3/Ch21_Lion/LioN/AddEditViewController.swift
@@ -0,0 +1,51 @@
+//
+//  AddEditViewController.swift
+//  LioN
+//
+//  Created by Wendy Wise on 9/24/16.
+//  Copyright © 2016 WisaAbility. All rights reserved.
+//
+
+import UIKit
+
+class AddEditViewController : UITableViewController, UITextFieldDelegate {
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var descTxt: UITextField!
+    @IBAction func doneClicked(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelClicked(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if (textField == nameTxt){
+            nameTxt.resignFirstResponder()
+            descTxt.becomeFirstResponder()
+        }else {
+            descTxt.resignFirstResponder()
+        }
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameTxt.becomeFirstResponder()
+        let tap : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard(){
+        view.endEditing(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
