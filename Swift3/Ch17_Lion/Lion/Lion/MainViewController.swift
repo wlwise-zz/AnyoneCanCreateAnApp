f4545c89d499d7f7977058d6fe03409c3bc5438b
Wendy Wise <wendylwise@gmail.com>
2016-09-24 10:39:12 -0400

    updated chapter 17 for swift 3

---
+++ Swift3/Ch17_Lion/Lion/Lion/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  LioN
+//
+//  Created by Wendy Wise on 9/24/16.
+//  Copyright © 2016 WisaAbility. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "lionCell", for: indexPath);
+        cell.textLabel?.text = "my first lion"
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
