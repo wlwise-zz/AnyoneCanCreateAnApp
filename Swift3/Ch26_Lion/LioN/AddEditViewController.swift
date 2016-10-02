d1529700476f2dcb1fe40a07e8708b7d6a78cd7b
Wendy Wise <wendylwise@gmail.com>
2016-10-02 12:21:47 -0400

    updated for swift 3

---
+++ Swift3/Ch26_Lion/LioN/AddEditViewController.swift
@@ -0,0 +1,133 @@
+//
+//  AddEditViewController.swift
+//  LioN
+//
+//  Created by Wendy Wise on 10/1/16.
+//  Copyright © 2016 WisaAbility. All rights reserved.
+//
+
+import UIKit
+protocol AddEditViewControllerDelegate : class {
+    func addItemViewControllerDidCancel(controller: AddEditViewController)
+    func addItemViewController(controller: AddEditViewController, didFinishAddingItem lionItem: Lion)
+    func addItemViewController(controller: AddEditViewController, didFinishEditingItem lionItem: Lion)
+}
+
+class AddEditViewController : UITableViewController, UITextFieldDelegate {
+    weak var delegate: AddEditViewControllerDelegate?
+
+    var lionDetail : Lion?
+    var lionToEdit : Lion?
+
+    var newLion: Lion?
+    var likeVar: Int = 1 //1 is like, and 0 is dislike
+
+    @IBOutlet weak var likeButton: UIButton!
+
+    @IBOutlet weak var dislikeButton: UIButton!
+
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    @IBOutlet weak var descTxt: UITextField!
+    
+    @IBAction func likeClicked(_ sender: AnyObject) {
+        likeVar = 1
+        toggleLike()
+    }
+    @IBAction func dislikeClicked(_ sender: AnyObject) {
+        likeVar = 0
+        toggleLike()
+    }
+
+    
+    
+    @IBAction func doneClicked(){
+        if let item = lionToEdit{
+            item.like = likeVar
+            item.lionDescription = descTxt.text!
+            item.lionName = nameTxt.text!
+            delegate?.addItemViewController(controller: self, didFinishEditingItem: item)
+
+        }else{
+            newLion = Lion()
+            newLion?.lionName = nameTxt.text!
+            newLion?.lionDescription = descTxt.text!
+            newLion?.like = likeVar
+            delegate?.addItemViewController(controller: self, didFinishAddingItem: newLion!)
+        }
+
+
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText : NSString = textField.text! as NSString
+        let newText : NSString = oldText.replacingCharacters(in: range, with: string) as NSString
+        doneButton.isEnabled = (newText.length > 0)
+        return true
+    }
+
+    
+    @IBAction func cancelClicked(){
+        delegate?.addItemViewControllerDidCancel(controller: self)
+
+    }
+
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField == nameTxt){
+            nameTxt.resignFirstResponder()
+            descTxt.becomeFirstResponder()
+        }else {
+            descTxt.resignFirstResponder()
+        }
+        return true
+    }
+
+    func dismissKeyboard(){
+        view.endEditing(true)
+    } 
+
+    func toggleLike(){
+        if likeVar == 1 {
+            dislikeButton.setImage(UIImage(named: "orNot"), for: .normal)
+            likeButton.setImage(UIImage(named: "likeItSelected"), for: .normal)
+        }else {
+            dislikeButton.setImage(UIImage(named: "orNotSelected"), for: .normal)
+            likeButton.setImage(UIImage(named: "likeIt"), for: .normal)
+        }
+    }
+
+
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        doneButton.isEnabled = false
+        if let item = lionToEdit {
+            title = "Edit Lion"
+            descTxt.text = item.lionDescription
+            nameTxt.text = item.lionName
+            likeVar = item.like
+            doneButton.isEnabled = true
+        } else {
+            title = "Add Lion"
+        }
+        toggleLike()
+        nameTxt.becomeFirstResponder()
+        let tap : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+    }
+
+
+
+}
---
+++ Swift3/Ch26_Lion/LioN/MainViewController.swift
@@ -0,0 +1,144 @@
+//
+//  ViewController.swift
+//  LioN
+//
+//  Created by Wendy Wise on 10/1/16.
+//  Copyright © 2016 WisaAbility. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController, AddEditViewControllerDelegate  {
+    var lionData : [Lion] = []
+    var editIndexPath: NSIndexPath?
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        lionData = [Lion]()
+        loadLions()
+
+    }
+    func loadLions(){
+        let path = getDataFilePath()
+        if let data =  try? Data(contentsOf: path){
+            let unarchiver = NSKeyedUnarchiver(forReadingWith: data)
+            lionData = unarchiver.decodeObject(forKey: "lionData") as! [Lion]
+            unarchiver.finishDecoding()
+        }
+    }
+    
+    func saveLionItems() {
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWith: data)
+        archiver.encode(lionData, forKey: "lionData")
+        archiver.finishEncoding()
+        data.write(to: getDataFilePath(), atomically: true)
+    }
+    
+    func addItemViewControllerDidCancel(controller: AddEditViewController) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+    func addItemViewController(controller: AddEditViewController, didFinishAddingItem lionItem: Lion) {
+        addItem(lionItem: lionItem)
+        dismiss(animated: true, completion: nil)
+        saveLionItems()
+    }
+    
+    func addItemViewController(controller: AddEditViewController, didFinishEditingItem lionItem: Lion) {
+        if let cell = tableView.cellForRow(at: editIndexPath! as IndexPath){
+            cell.textLabel?.text = lionItem.lionName
+            cell.detailTextLabel?.text = lionItem.lionDescription
+            
+            lionData[editIndexPath!.row].lionDescription = lionItem.lionDescription
+            lionData[editIndexPath!.row].lionName = lionItem.lionName
+            lionData[editIndexPath!.row].like = lionItem.like
+            if (lionData[editIndexPath!.row].like == 1){
+                cell.imageView!.image = UIImage(named: "likeItCell")
+                
+            }else {
+                cell.imageView!.image = UIImage(named: "orNotCell")
+            }
+        }
+        saveLionItems()
+        dismiss(animated: true, completion: nil)
+    }
+
+    
+    func addItem(lionItem :  Lion) {
+        let currentIndex = lionData.count
+        lionData.append(lionItem)
+        let indexPath = IndexPath(row: currentIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "add" {
+            let navigationController = segue.destination as! UINavigationController
+            let controller = navigationController.topViewController as! AddEditViewController
+            controller.delegate = self
+        }else if segue.identifier == "edit" {
+            let navigationController = segue.destination as! UINavigationController
+            let controller = navigationController.topViewController as! AddEditViewController
+            controller.delegate = self
+            if let indexPath = tableView.indexPath(for: sender as! UITableViewCell){
+                controller.lionToEdit = lionData[indexPath.row]
+                editIndexPath = indexPath as NSIndexPath?
+            }
+        }
+    }
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lionData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        lionData.remove(at: indexPath.row)
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "lionCell", for: indexPath);
+
+        cell.textLabel?.text = lionData[indexPath.row].lionName
+        cell.detailTextLabel?.text = lionData[indexPath.row].lionDescription
+        if lionData[indexPath.row].like == 1{
+            cell.imageView?.image =   UIImage(named: "likeItCell")
+        }else{
+            cell.imageView?.image =  UIImage(named: "orNotCell")
+        }
+
+        return cell
+
+    }
+    
+    func getDataFilePath() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0].appendingPathComponent("Lion.plist")
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(getDataFilePath())
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
